41284f9d4a4d89e413928d21bd200f77e24d6edf
Nyina <dobinyina@gmail.com>
2018-01-17 12:23:32 +0100

    Add files via upload

---
+++ ListingToDos/ListingToDos/Controllers/TodoController .cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ListingToDos.Models;
+using ListingToDos.Repositories;
+
+namespace ListingToDos.Controllers
+{
+    [Route("")]
+    public class TodoController : Controller
+    {
+        private ToDoRepository toDoRepository;
+
+        public TodoController (ToDoRepository toDoRepository)
+        {
+            this.toDoRepository = toDoRepository;
+        }
+
+        [HttpGet("/")]
+        //Index action, hogy egyből átdobjon a todo-ra
+        public IActionResult Index()
+        {
+            return Redirect("/todo");
+        }
+
+
+        [HttpGet("/todo")]
+        //Ha az Action neve azonos lenne egy másikkal (itt: List), akkor paraméterben 1-nek meg kell adni ""-be a Listet, utána pedig a methodot
+        public IActionResult ToDo()
+        {
+            return View("List", toDoRepository.ShowList());
+        }
+
+        //[Route("/list")]
+        //public IActionResult List()
+        //{
+        //    //A . után be kell hivatkozni a repository listát funkcióját
+        //    return View(toDoRepository.ShowList());
+        //}
+
+        [Route("/list")]
+        public IActionResult List([FromQuery]bool isActive)
+        {
+            //Paraméterben kell megadni a szűrési feltételt: (active means !isDone): http://localhost:5000/todo/?isActive=true
+            //A repositoryban, ahogy a methodokat eddig is csináltuk
+
+            //Itt már a decide methodot használjuk
+            return View(toDoRepository.DecideListType(isActive));
+        }
+
+        [HttpGet("/todo/add")]
+        public IActionResult ViewAddForm()
+        {
+            return View("Add");
+        }
+
+        [HttpPost("/todo/add")]
+        public IActionResult CreateListElement(ToDo toDo)
+        {
+            toDoRepository.CreateNewListItem(toDo);
+            return Redirect("/todo");
+        }
+
+        [HttpGet("/{id}/delete")]
+        public IActionResult Delete(long id)
+        {
+            toDoRepository.DeleteAListItem(id);
+            return Redirect("/todo");
+            //return RedirectToAction("List");
+        }
+
+        [HttpGet("/{id}/edit")]
+        public IActionResult Edit(long id)
+        {
+            //Ha id van az URL-ben, akkor azt kell paraméterbe is beadni (a deletnél is úgy volt!)
+            //Úgy kell megadni, mint a ToDo methodnál, csak mivel a method nevével (Edit) egyezik a View neve, nem kell ez utóbbit odaírni már
+            return View(toDoRepository.GetAListItem(id));
+        }
+
+        [HttpPost("/{id}/edit")]
+        public IActionResult CreateListElement(ToDo toDo, long id)
+        {
+            toDoRepository.EditAListItem(toDo, id);
+            return Redirect("/todo");
+        }
+    }
+}
---
+++ ListingToDos/ListingToDos/Repositories/ToDoRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ListingToDos.Entities;
+using ListingToDos.Models;
+
+namespace ListingToDos.Repositories
+{
+    public class ToDoRepository
+    {
+        private ToDoContext toDoContext;
+
+        public ToDoRepository (ToDoContext toDoContext)
+        {
+            this.toDoContext = toDoContext;
+        }
+
+        //ez a lista nem kell, csak a kiíratást néztük meg vele
+        //public List<ToDo> todoList = new List<ToDo>()
+        //{
+        //   new ToDo() { Title = "Start the day" },
+        //   new ToDo() { Title = "Finish H2 workshop1" },
+        //   new ToDo() { Title = "Finish JPA workshop2" },
+        //   new ToDo() { Title = "Create a CRUD project" }
+        //};
+
+        public List<ToDo> ShowList()
+        {
+            //A context todo dbset-jét listává kell alakítani
+            //és eztv a controllerben meg kell hívni
+            return toDoContext.ToDos.ToList();
+        }
+
+        public List<ToDo> ShowListOfIsActive()
+        {
+            return toDoContext.ToDos.Where(t => t.IsDone == false).ToList();
+        }
+
+        public List<ToDo> DecideListType(bool IsActive)
+        {
+            if (IsActive)
+            {
+                return ShowListOfIsActive();
+            }
+            else
+            {
+                return ShowList();
+            }
+        }
+
+        public void CreateNewListItem(ToDo ToDo)
+        {
+            toDoContext.ToDos.Add(ToDo);
+            toDoContext.SaveChanges();
+        }
+
+        public void DeleteAListItem(long index)
+        {
+            //ToDo t = toDoContext.ToDos.Find(index);
+            ToDo t = toDoContext.ToDos.FirstOrDefault(x => x.Id == index);
+            toDoContext.ToDos.Remove(t);
+            toDoContext.SaveChanges();
+        }
+
+        public void EditAListItem(ToDo todo, long index)
+        {
+            ToDo t = toDoContext.ToDos.FirstOrDefault(x => x.Id == index);
+            t.Title = todo.Title;
+            t.IsDone = todo.IsDone;
+            t.IsUrgent = todo.IsUrgent;
+            toDoContext.SaveChanges();
+        }
+
+        public ToDo GetAListItem(long index)
+        {
+            //1 db todo-t adunk vissza
+            return toDoContext.ToDos.FirstOrDefault(x => x.Id == index);
+        }
+
+    }
+}
